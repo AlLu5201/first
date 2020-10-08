2728baab2785f51623cb83e1b57a8b97a68759b6
val <valLoukach@gmail.com>
2020-10-08 15:57:27 -0400

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by val on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
