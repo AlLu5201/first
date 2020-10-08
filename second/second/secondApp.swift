4961b8f0ad8a1f5400e8bd3d2ec4c1e2bb8a8e60
val <valLoukach@gmail.com>
2020-10-08 16:05:53 -0400

    second commit

---
+++ second/second/secondApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondApp.swift
+//  second
+//
+//  Created by val on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct secondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
