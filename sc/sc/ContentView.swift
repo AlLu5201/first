e11e6fecd7eb225ab7b39339bf4bdaa262a353ff
val <valLoukach@gmail.com>
2020-10-10 10:24:55 -0400

    c2

---
+++ sc/sc/ContentView.swift
@@ -0,0 +1,15 @@
+//c1
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, sc!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sc/sc/scApp.swift
@@ -0,0 +1,17 @@
+//
+//  scApp.swift
+//  sc
+//
+//  Created by val on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct scApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
